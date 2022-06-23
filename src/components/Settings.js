7c050fe58478a006b6453385274f02213c733f72
Errica <errica.cheng@gmail.com>
2022-06-22 23:01:33 -0400

    unit selection added
    Unit selection is also performed using React-Select in order to populate the list with objects and more easily retrieve the data required.
    
    Because React-Select lacks field validation (which took me way too long to figure out), I set both the default participant values to the first unit and made the fields unable to be cleared. Figuring out the exact value to input took... longer than it should have. I really wish we practiced more file/data reading in classes :')
    
    While the current React-Select setup means units can fight against themselves, it's not a big deal (ie having the forms check one another is a very, very low priority right now).
    
    ResultDisplay temporarily added into Settings.js; may keep it there so global variables are easier to deal with.
    
    May remove Fight! button if the React components update instantly as they do in my brain. We'll deal with that tomorrow - probably making a dummy output to determine the correct inputs.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,8 @@
 import Select from "react-select";
+import ReactTooltip from "react-tooltip";
 
 const CombatArtsData = require('../props/combat_arts.json');
+const UnitsData = require('../props/unitsTemp.json');
 
 function Settings() {
     return (
@@ -9,15 +11,76 @@ function Settings() {
             <form action="">
                 <label htmlFor='participant1'>Participant 1: </label>
                 <br />
-                <input type='text' id="participant1" placeholder="Enter name here" className="textbox"
-                       autoComplete="off"/><br/>
+                <div style={{width: '242px', margin: '0 2.5rem 2rem 2.5rem'}}>
+                    <Select //menuIsOpen = {true}
+                        options={UnitsData}
+                        defaultValue={UnitsData[0].options[0]}
+                        getOptionLabel={(option) => `${option.name}`}
+                        theme={(theme) => ({
+                            ...theme,
+                            baseUnit: 50,
+                            borderRadius: 4,
+                            borderColor: '#64C89A',
+                            boxShadow: 0,
+                            colors: {
+                                ...theme.colors,
+                                text: 'black',
+
+                                neutral0: '#64C89A',
+                                neutral5: '#64C89A',
+                                neutral10: '#8FF2B6',
+                                neutral20: '#8FF2B6',
+                                neutral30: '#64C89A',
+                                neutral40: '#8FF2B6',
+                                neutral50: 'black',
+                                neutral60: '#8FF2B6',
+                                neutral80: '#8FF2B6',
+
+                                primary: '#8FF2B6', //color of option, after selected
+                                primary25: '#8FF2B6', //color while hovering, before selected
+                                primary50: '#8FF2B6', //color while clicking, before selected
+                                primary75: 'black'
+                            },
+                        })}
+                    />
+                </div>
 
                 <label htmlFor='participant2'>Participant 2: </label>
                 <br />
-                <input type='text' id="participant2" placeholder="Enter name here" className="textbox"
-                       autoComplete="off"/><br/>
+                <div style={{width: '242px', margin: '0 2.5rem 2rem 2.5rem'}}>
+                    <Select //menuIsOpen = {true}
+                        options={UnitsData}
+                        defaultValue={UnitsData[0].options[0]}
+                        getOptionLabel={(option) => `${option.name}`}
+                        theme={(theme) => ({
+                            ...theme,
+                            baseUnit: 50,
+                            borderRadius: 4,
+                            borderColor: '#64C89A',
+                            boxShadow: 0,
+                            colors: {
+                                ...theme.colors,
+                                text: 'black',
 
-                <label htmlFor='house'>Combat Art: </label>
+                                neutral0: '#64C89A',
+                                neutral5: '#64C89A',
+                                neutral10: '#8FF2B6',
+                                neutral20: '#8FF2B6',
+                                neutral30: '#64C89A',
+                                neutral40: '#8FF2B6',
+                                neutral50: 'black',
+                                neutral60: '#8FF2B6',
+                                neutral80: '#8FF2B6',
+
+                                primary: '#8FF2B6', //color of option, after selected
+                                primary25: '#8FF2B6', //color while hovering, before selected
+                                primary50: '#8FF2B6', //color while clicking, before selected
+                                primary75: 'black'
+                            },
+                        })}
+                    /></div>
+
+                <label htmlFor='combatArt'>Combat Art: </label>
                 <br />
                 <div style={{width: '242px', margin: '0 2.5rem 2rem 2.5rem'}}>
                     <Select //menuIsOpen = {true}
@@ -60,5 +123,79 @@ function Settings() {
         </div>
     );
 }
+
+// function ResultDisplay(props) {
+//     return (
+//         <div className="Results">
+//             <div className="Participant1">
+//                 <div className="UnitIconContainer">
+//                     <div>
+//                         <img src={process.env.PUBLIC_URL + '/img/sprite/' + props.unit1.name + '.png'} className="unit-icon" alt={participant1} data-tip={p1Tip}/>
+//                     </div>
+//                     <div className="cwe-icons">
+//                         <img src={process.env.PUBLIC_URL + '/img/crest/aubin.png'} className="crest-icon" alt={abilityName} data-tip="Aubin (2): yeah"/>
+//                         <br />
+//                         <img src={process.env.PUBLIC_URL + '/img/wep/swordd.png'} className="wep-icon" alt={abilityName} data-tip="funny sword"/>
+//                         <br />
+//                         <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="eqp-icon" alt={abilityName} data-tip="equipment"/>
+//                     </div>
+//                 </div>
+//                 <br />
+//
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <br />
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <br />
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//             </div>
+//
+//             <div className="Calculations">
+//                 <p>{participant1} attacks {participant2} with WEAPON LOL!</p>
+//             </div>
+//
+//             <div className="Participant2">
+//                 <div className="UnitIconContainer">
+//                     <div>
+//                         <img src={process.env.PUBLIC_URL + '/img/sprite/' + participant2 + '.png'} className="unit-icon" alt={participant2} data-tip={p2Tip}/>
+//                     </div>
+//                     <div className="cwe-icons">
+//                         <img src={process.env.PUBLIC_URL + '/img/crest/none.png'} className="crest-icon" alt={abilityName} data-tip="CRESTLESS BUFFOON"/>
+//                         <br />
+//                         <img src={process.env.PUBLIC_URL + '/img/wep/lanced.png'} className="wep-icon" alt={abilityName} data-tip="uhhh funny lance"/>
+//                         <br />
+//                         <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="eqp-icon" alt={abilityName} data-tip="equipment"/>
+//                     </div>
+//                 </div>
+//                 <br />
+//
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <br />
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <br />
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//                 <img src={process.env.PUBLIC_URL + '/img/abilities/' + abilityIcon + '.png'} className="ability-icon" alt={abilityName} data-tip={abilityDesc}/>
+//             </div>
+//
+//             <ReactTooltip className="toolTip" html={true} />
+//         </div>
+//     );
+// }
 
 export default Settings;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/css/style.css';
-import ResultDisplay from './components/ResultDisplay';
+// import ResultDisplay from './components/ResultDisplay';
 import Settings from './components/Settings';
 
 ReactDOM.render(
     <React.StrictMode>
         <Settings />
+        {/*<ResultDisplay />*/}
     </React.StrictMode>,
     document.getElementById('battle-settings')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ResultDisplay />
-  </React.StrictMode>,
-  document.getElementById('battle-outcome')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ResultDisplay />
+//   </React.StrictMode>,
+//   document.getElementById('battle-outcome')
+// );
