cd7335929210f7dcb6edcb6caf7cc61b452020f3
Errica <errica.cheng@gmail.com>
2022-06-30 14:43:26 -0400

    Separated calculations into own file

---
+++ src/functions/Calculations.js
@@ -0,0 +1,24 @@
+const UnitsData = require("../props/unitsTemp.json");
+
+/**
+ * @param unit
+ * @returns {number[]}
+ *
+ * The second-most unfortunate for(for(if()) garbage I've had to write. possibly clean up someday
+ * O(h*u), with u being number of units per house (~15 max) and h being number of houses (5)
+ *
+ */
+export function getUnitInd(unit) {
+    for(let houseIndex=0; houseIndex < UnitsData.length; houseIndex++) {
+        for(let unitIndex=0; unitIndex < UnitsData.length; unitIndex++) {
+            if(UnitsData[houseIndex].options[unitIndex].name === unit) {
+                return [houseIndex, unitIndex];
+            }
+        }
+    }
+}
+
+export function getUnitWep(unit) {
+    let unitIndArr = getUnitInd(unit);
+    return UnitsData[unitIndArr[0]].options[unitIndArr[1]].weapon;
+}
