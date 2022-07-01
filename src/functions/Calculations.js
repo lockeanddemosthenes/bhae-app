1155000f607b0e393358c2c2b74f8abe5648ebd7
Errica <errica.cheng@gmail.com>
2022-07-01 19:33:01 -0400

    fix calculation for unit index array
    in the for loop, i forgot i gotta look through each MEMBER of the houses, not each house

--- src/functions/Calculations.js
+++ src/functions/Calculations.js
@@ -4,13 +4,13 @@ const UnitsData = require("../props/unitsTemp.json");
  * @param unit
  * @returns {number[]}
  *
- * The second-most unfortunate for(for(if()) garbage I've had to write. possibly clean up someday
+ * The second-most for(for(if()) garbage I've had to write. possibly clean up someday
  * O(h*u), with u being number of units per house (~15 max) and h being number of houses (5)
  *
  */
 export function getUnitInd(unit) {
     for(let houseIndex=0; houseIndex < UnitsData.length; houseIndex++) {
-        for(let unitIndex=0; unitIndex < UnitsData.length; unitIndex++) {
+        for(let unitIndex=0; unitIndex < UnitsData[houseIndex].options.length; unitIndex++) {
             if(UnitsData[houseIndex].options[unitIndex].name === unit) {
                 return [houseIndex, unitIndex];
             }
